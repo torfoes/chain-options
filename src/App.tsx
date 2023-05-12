2dfbcea8863892f8775ab66507ce9c15f602f651
karlos <karloszuru@gmail.com>
2023-05-12 19:34:07 +0100

    Skimpped joy-ui template header for styling and whatnot

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useGlobalState} from "./utils/GlobalState";
+import Layout from "./components/Layout";
+
+
+import {
+    Box,
+    Button,
+    CssBaseline,
+    CssVarsProvider, IconButton, Input, Typography,
+} from '@mui/joy';
+import {Menu} from "@mui/icons-material";
+
+
 
 function App() {
+  const [state, dispatch] = useGlobalState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <CssVarsProvider defaultMode="dark">
+        <CssBaseline />
+          <Layout.Header>
+              <IconButton variant={'plain'} color={'neutral'}>
+                  <Menu/>
+              </IconButton>
+              <Box sx={{ flexGrow: 0, display: 'flex', justifyContent: 'flex-start' }}>
+                  <Typography level='h5' variant={'plain'} color={'neutral'}>Block Explorer</Typography>
+              </Box>
+
+              <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}> {/* Add this wrapper */}
+                  <Input
+                      size="md"
+                      placeholder="Search by block / transaction / address…"
+                      // startDecorator={<SearchRoundedIcon color="disabled" />}
+                      sx={{
+                          flexBasis: '500px',
+                          display: {
+                              xs: 'none',
+
+                              sm: 'flex',
+                          },
+                      }}
+                  />
+              </Box>
+              <Button/>
+          </Layout.Header>
+
+
+      </CssVarsProvider>
   );
 }
 
